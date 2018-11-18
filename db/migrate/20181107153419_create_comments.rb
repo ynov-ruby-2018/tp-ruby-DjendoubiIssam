15f524200783f56f2dd4ef1a0fe7518b9a9bbdaa
Guillaume Barillot <gbarillot@gmail.com>
2018-11-18 16:29:13 +0100

    Update 20181107153419_create_comments.rb

--- db/migrate/20181107153419_create_comments.rb
+++ db/migrate/20181107153419_create_comments.rb
@@ -3,6 +3,7 @@ class CreateComments < ActiveRecord::Migration[5.2]
     create_table :comments do |t|
       t.timestamps
       t.text :content
+      # Donc, aucune relation entre le User et le Comment ? Il aurait fallu ajouter un user_id pour stocker la clé étrangère
     end
   end
 end
