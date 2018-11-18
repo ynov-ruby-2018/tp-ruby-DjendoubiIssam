c41bd5defc689ad0671b364b00ebcfa511fe68c5
Guillaume Barillot <gbarillot@gmail.com>
2018-11-18 16:31:12 +0100

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
     devise_for :users
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-    resources :home
+    resources :home # Sans only: [:index....] vous laissez des routes ouvertes pour rien :-/
 
-    root to: 'home#index'
+    root to: 'home#index' # Idéalement, devrait être la première route, tout en haut. 
 
   end
